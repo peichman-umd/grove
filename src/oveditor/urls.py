39d29edb2db7b759df9162d7e9b94507595ff2c0
Peter Eichman <peichman@umd.edu>
2024-02-26 10:11:15 -0500

    Redirect / to /vocabs

--- src/oveditor/urls.py
+++ src/oveditor/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.http import HttpResponseRedirect
+from django.urls import include, path, reverse
 
 urlpatterns = [
+    path('', lambda _: HttpResponseRedirect(reverse('list_vocabularies'))),
     path('admin/', admin.site.urls),
     path('vocabs/', include('vocabs.urls'))
 ]
